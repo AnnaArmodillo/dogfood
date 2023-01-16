1d5a3515ed34c426276c416953358c63c841a01c
Anna Denisova <abachledacurus@gmail.com>
2023-01-16 18:35:12 +0700

    Реализовано появление поля поиска по клику

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import classNames from 'classnames';
+import { func } from 'prop-types';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../Components/logo.jpg';
 import headerStyle from './header.module.css';
 export function Header() {
+    console.log('render header');
+    const [isSearchActive, setIsSearchActive] = useState(false);
+    function clickSearchHandler() {
+        setIsSearchActive(true);
+    }
+    function closeSerachHandler() {
+        setIsSearchActive(false);
+    }
     return (
         <ul className={headerStyle.header}>
             <li>
@@ -18,21 +28,24 @@ export function Header() {
                     DogFood
                 </NavLink>
             </li>
-            <li>
-                <div className={headerStyle.searchWrapper}>
-                    <div
-                        contentEditable={true}
-                        suppressContentEditableWarning={true}
-                        className={headerStyle.search}
-                        type='text'
-                    ></div>
-                    <i
-                        className={classNames(
-                            'fa-solid fa-circle-xmark',
-                            headerStyle.searchCloseButton
-                        )}
-                    ></i>
-                </div>
+            <li>{isSearchActive ? <div className={headerStyle.searchWrapper}>
+                <div
+                    contentEditable={true}
+                    suppressContentEditableWarning={true}
+                    className={headerStyle.search}
+                    type='text'
+                ></div>
+                <i onClick={closeSerachHandler}
+                    className={classNames(
+                        'fa-solid fa-circle-xmark',
+                        headerStyle.searchCloseButton
+                    )}
+                ></i>
+            </div> : <i onClick={clickSearchHandler} className={classNames(
+                'fa-solid fa-magnifying-glass',
+                headerStyle.searchOpenButton
+            )}></i>}
+
             </li>
             <li>
                 <NavLink
